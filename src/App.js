1c2cd6e50dc3fd355dc667941659edb957543fe5
guy.marushchenko <guy.marushchenko@fiverr.com>
2022-02-15 15:29:03 +0200

    wiop

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import React from "react";
 import "./App.css";
 import Button from "@mui/material/Button";
 import { sha1 } from "crypto-hash";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Callback from "./Callback";
+import Cookies from "universal-cookie";
 
 function App() {
   const clientId = "kbyuFDidLLm280LIwVFiazOqjO3ty8KH";
   const redirectUrl = "https://guymarush.github.io/fiverr_sso_poc/callback";
   const scope = "openid%20profile%20email";
   const responseType = "code";
+  let state = "";
+  let nonce = "";
 
   const handleClick = async () => {
-    const state = "e7199af2c091e7672917c390e3faa709bbc88b81";
-    const nonce = await sha1("nonce secret");
+    state = await sha1("state secret");
+    nonce = await sha1("nonce secret");
+
+    const cookies = new Cookies();
+    cookies.set("state", state);
+    cookies.set("nonce", nonce);
 
     const data = await fetch(
       `https://accounts.dev.fiverr.com/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUrl}&scope=${scope}&response_type=${responseType}&state=${state}&nonce=${nonce}`
@@ -20,12 +30,20 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button variant="contained" onClick={handleClick}>
-          Connect with Fiverr
-        </Button>
-      </header>
+    <div className="App-body">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/callback" element={<Callback state={state} />} />
+          <Route
+            path="/"
+            element={
+              <Button variant="contained" onClick={handleClick}>
+                Connect with Fiverr
+              </Button>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Callback.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
+const Callback = (props) => {
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    const code = searchParams.get("code");
+    const state = searchParams.get("state");
+
+    if (props.state !== state) {
+      //stop here, state values are not identical
+      console.log("not identical state");
+      return;
+    }
+
+    console.log("identical state");
+
+    const response = test;
+  }, []);
+
+  return searchParams.get("code");
+};
+
+export default Callback;
